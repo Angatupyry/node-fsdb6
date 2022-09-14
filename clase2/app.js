9a29756189ecfa482cb41cd2f49be8a02424876c
Angatupyry <fierrofenix@gmail.com>
2022-09-14 10:42:46 -0400

    Clase 2. Swapi

--- clase2/app.js
+++ clase2/app.js
@@ -1,5 +1,6 @@
 //1 Llamando al paquete para utilizarlo
 const express = require("express");
+const axios = require("axios");
 // - 2 Inicializar o instanciar express en una variable. (Por convención llamada app)
 const app = express();
 
@@ -14,6 +15,51 @@ app.get("/ronaldinho", (req, res) => {
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/swapi", async (req, res) => {
+  try {
+    const characters = await axios("https://swapi.dev/api/people/");
+
+    return res.status(200).json(characters.data.results);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get("/swapi/:personaje_id", async (req, res) => {
+  try {
+    const data = {
+      personaje: {},
+      planeta: {},
+    };
+
+    const personaje_id = req.params.personaje_id;
+
+    const characterAxios = await axios(
+      `https://swapi.dev/api/people/${personaje_id}`
+    );
+
+    const character = characterAxios.data;
+
+    const planet = await axios(character.homeworld);
+
+    data.personaje = {
+      nombre: character.name,
+      altura: character.height,
+      color_ojo: character.eye_color,
+    };
+
+    data.planeta = {
+      nombre: planet.data.name,
+      gravedad: planet.data.gravity,
+      terreno: planet.data.terrain,
+    };
+
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 // 5 - Levantar el servidor
